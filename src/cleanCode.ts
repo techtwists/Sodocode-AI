8014ca22e5036a654e0fd63f33bed995ad9e3466
TechTwists <techtwists@gmail.com>
2023-03-08 22:23:10 +0600

    v0.0.9

--- src/cleanCode.ts
+++ src/cleanCode.ts
@@ -17,8 +17,9 @@ export async function codeAI():Promise<string>{
 
   const getCode = await generateCodeFromPseudocode(pseudocode);
   const codeclean1 = getCode.replace(/&lt;/g, "<");
-  const codeclean2 = codeclean1.replace(/\r\n\r\n/g, "\n");
-  const code = codeclean2.replace(/\r\n/g, "\n");
+  const codeclean2 = codeclean1.replace(/&gt;/g, ">");
+  const codeclean3 = codeclean2.replace(/\r\n\r\n/g, "\n");
+  const code = codeclean3.replace(/\r\n/g, "\n");
 
   return code;
 }
